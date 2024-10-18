1abbb4e613ec6ccfd159546b9f5abbe0a0bb4cc5
nehal <nehalhomoda00@gmail.com>
2024-10-18 13:09:30 +0300

    first commit

---
+++ components/user/cards/ProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card color="deep-orange-lighten-3" variant="outlined" class="d-flex flex-column justify-center align-center"  rounded="large">
+     <v-img width="30" height="30" :src="image"></v-img>
+     <p class="text-black">{{ name }}</p>
+    </v-card>
+</template>
+<script setup lang="ts">
+const props=defineProps(['image','name'])
+
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+ <v-sheet  class="py-16">
+    <v-container>
+        <v-row>
+          <v-col cols="12" md="4" v-for="(item, index) in 5"  :key="index">
+            <UserCardsProductCard
+              name="car"
+              image="/assets/imgs/hero/avatar.png"
+            />
+          </v-col>
+          </v-row>
+
+    </v-container>
+ </v-sheet>
+ 
+</template>
+<script setup lang="ts">
+</script>
+
+
+<style lang="scss" scoped></style>
