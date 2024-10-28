172067a02140e08cb54d2d6ca1a0bf5fbbf09442
nehal <nehalhomoda00@gmail.com>
2024-10-28 11:52:26 +0300

    deal with snackbar

---
+++ components/shared/AppNavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-app-bar>
+            <v-btn @click="login" rounded="lg" variant="outlined" elevation="0" color="deep-orange-lighten-3" >
+                login
+            </v-btn>
+        </v-app-bar>
+    </div>
+
+
+
+    <template>
+  <div class="text-center">
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+    >
+    Thank You,You are logged in
+
+      <template v-slot:actions>
+        <v-btn
+          color="blue"
+          variant="text"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+</template>
+
+<script setup lang="ts">
+const isLoggedIn=ref(false)
+const snackbar=ref(false)
+const timeout=ref(2000)
+const login=()=>{
+    isLoggedIn.value=true
+    snackbar.value=true
+}
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+// import { SharedAppNavBar } from '#build/components';
+</script>
+
 <template>
     <v-app>
      <v-main>
+      <SharedAppNavBar/>
         <slot/>
      </v-main>
     </v-app>
